d9c718f85f7130ae83894c6cb17b225363dfbf88
Kevin Huang <yellow.khaos@gmail.com>
2018-07-15 03:25:47 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
-      @user = User.find_by(name: params[:name])
-      if @user.try(authenticate: params[:password])
+      @user = User.find_by(name: params[:user][:name])
+
+      if @user.try(:authenticate, params[:user][:password])
         session[:user_id] = @user.id
+        redirect_to home_path
       else
         redirect_to login_path
       end
