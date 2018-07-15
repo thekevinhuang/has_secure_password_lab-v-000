523788676eee7a0a9f813a582c6b3aad433bae15
Kevin Huang <yellow.khaos@gmail.com>
2018-07-15 03:10:58 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,12 @@ class SessionsController < ApplicationController
 
   def create
       @user = User.find_by(name: params[:name])
-
-      if @user.try(authenticate: params[:password])
-        session[:user_id] = @user.id
-      else
-        redirect_to login_path
-      end
+      
+      #if @user.try(authenticate: params[:password])
+      #  session[:user_id] = @user.id
+      #else
+      #  redirect_to login_path
+      #end
   end
 
 end
