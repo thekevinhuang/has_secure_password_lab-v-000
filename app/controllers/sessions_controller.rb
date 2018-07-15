df9f5b63eec4e69fff20ab21f330b3c7f34e2ebb
Kevin Huang <yellow.khaos@gmail.com>
2018-07-15 03:23:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-      @user = User.find_by(name: params[:name])
+      @user = User.find_by(name: params[:user][:name])
 
       binding.pry
 
